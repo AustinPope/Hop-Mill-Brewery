f87ab6116efc19c172d16f9307075cc01285cea4
Eric <ekiesow@mail.csuchico.edu>
2020-10-29 12:45:13 -0700

    added error checking for empty fields

--- hop-mill/src/api.rs
+++ hop-mill/src/api.rs
@@ -19,7 +19,13 @@ pub struct Message {
 pub fn new_message(message_form: Form<Message>) -> Flash<Redirect> {
     // a forward or a failure can be caught by using the Option and Result
     let message: Message = message_form.into_inner();
-    if message.subject.is_empty() {
+    if message.name.is_empty() {
+        return Flash::error(Redirect::to("/contact"), "Name cannot be empty.");
+    } else if message.email_address.is_empty() {
+        return Flash::error(Redirect::to("/contact"), "Email Address cannot be empty.");
+    } else if message.phone_number.is_empty() {
+        return Flash::error(Redirect::to("/contact"), "Phone Number cannot be empty.");
+    } else if message.subject.is_empty() {
         return Flash::error(Redirect::to("/contact"), "Subject cannot be empty.");
     } else if message.message.is_empty() {
         return Flash::error(Redirect::to("/contact"), "Message cannot be empty.");
