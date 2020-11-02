01c4800a7bb47fbc2b9262e208679d56f07048bb
AustinPope <mraustinpope@gmail.com>
2020-11-01 18:42:14 -0800

    Base and routes html using terra templates for rust rocket

--- hop-mill/src/about.rs
+++ hop-mill/src/about.rs
@@ -1,4 +1,17 @@
+use rocket_contrib::templates::Template;
+use serde::Serialize;
+
 #[get("/about")]
-pub fn about() -> &'static str {
-    "About Hop Mill Brewery"
+pub fn about() -> Template {
+    #[derive(Serialize)]
+    struct Context {
+        first_name: String,
+        last_name: String,
+    }
+
+    let context = Context {
+        first_name: String::from("Rust"),
+        last_name: String::from("Rocket"),
+    };
+    Template::render("about", context)
 }
