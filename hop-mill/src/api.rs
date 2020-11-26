a1b1857eadd0dd741faefa08a12aef1f6bd8dbca
AustinPope <mraustinpope@gmail.com>
2020-11-25 17:08:30 -0800

    setting up credentials and sessions for db

--- hop-mill/src/api.rs
+++ hop-mill/src/api.rs
@@ -3,6 +3,7 @@ extern crate serde_json;
 use serde::{Serialize};
 use rocket::request::Form;
 use rocket::response::{Flash, Redirect};
+use firestore_db_and_auth::{Credentials, ServiceSession, documents, sessions};
 
 #[derive(FromForm, Debug, Serialize)]
 pub struct Message {
@@ -34,6 +35,10 @@ pub fn new_message(message_form: Form<Message>) -> Flash<Redirect> {
         return Flash::error(Redirect::to("/contact"), "Message cannot be empty.");
     }
     let serialized = serde_json::to_string(&message).unwrap();
+    let cred = Credentials::from_file("Hop-Mill-250e45068a78.json").expect("Read credentials file");
+    let session = ServiceSession::new(cred).expect("Create a service account session");
+    let _result = documents::write(&session, "messages", Some("mVIXrAqIC7a6pWNinzj8"),
+     &serialized, documents::WriteOptions::default());
     Flash::success(
         Redirect::to("/contact"),
         format!("Message successfully serialized {}", serialized)
