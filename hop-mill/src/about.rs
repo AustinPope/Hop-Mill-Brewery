c4c6608f8a7fa1cb846db9bece91a4ecc13328f7
eric <ekiesow@mail.csuchico.edu>
2020-11-28 14:20:15 -0800

    started about page, added images and styling

--- hop-mill/src/about.rs
+++ hop-mill/src/about.rs
@@ -5,13 +5,9 @@ use serde::Serialize;
 pub fn about() -> Template {
     #[derive(Serialize)]
     struct Context {
-        first_name: String,
-        last_name: String,
     }
 
     let context = Context {
-        first_name: String::from("Rust"),
-        last_name: String::from("Rocket"),
     };
     Template::render("about", context)
 }
